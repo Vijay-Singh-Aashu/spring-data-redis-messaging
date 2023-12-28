82fdd3cc35f7c382ca7b8a5c03d71ee7d9b3af4a
Vijay Singh <vijaysinghaashu@gmail.com>
2023-12-28 17:38:13 +0530

    initialize the project

---
+++ src/test/java/com/excellence/spring_redis_pubsub/SpringRedisPubsubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.spring_redis_pubsub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisPubsubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
